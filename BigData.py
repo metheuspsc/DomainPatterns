67b206fa7c4f448310fec29df00bbb6c0913b59c
Matheus.P <matheuspessoax@gmail.com>
2020-10-18 22:44:30 -0300

    refactor

--- BigData.py
+++ BigData.py
@@ -1,170 +1,41 @@
 import os
 import logging
 import json
-import pandas as pd
+from functions import *
 
-# These are the Domain File Paths
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M',
+                    level=logging.DEBUG,
+                    handlers=[logging.FileHandler('BigData.log'),
+                              logging.StreamHandler()])
 
 org_file = r'.\Domains\Org\org.txt'
 net_file = r'.\Domains\Net\net.txt'
 com_file = r'.\Domains\Com\com.txt'
 
-# This is a list with the Domain Files to be processed, if you want to process less than the three of them, just erase one or more from the list.
-
 domain_list = [org_file]
-
-# This is the folder holding the 12 Patters CSV files.
 
 pattern_folder = r'.\Patterns'
 
-# This is the lenght of the strings filtered on the Domain file since our largest pattern has 5 characthers the length is 5 + .org.(Or net or com) so it adds up to 10
 length = 10
-
-
-def chunk_processing(chunk):
-    ''' Processes chunks of the Domain file so as to reduce memory usage'''
-
-    chunk.drop_duplicates(inplace=True)
-
-    # Erases lines longer than the length we want
-    chunk = chunk[chunk.str.len() <= length]
-
-    # Erases the extension of the domain(.com.;.net.;.org.)
-    chunk = chunk.str[:-5]
-
-    return chunk
-
-
-def clean_domain_df(domain_file, domain_file_basename, domain_folder):
-    '''This function cleans the domain file, saving a CSV and returning a dataframe.'''
-
-    domain_df = pd.DataFrame()
-
-    for chunk in pd.read_csv(domain_file, sep='\t', usecols=[0], dtype='category', chunksize=200000, squeeze=True):
-
-        chunk = chunk_processing(chunk)
-
-        domain_df = pd.concat([domain_df, chunk], ignore_index=True, axis=0)
-
-    domain_df.drop_duplicates(inplace=True)
-
-    # Saves results to CSV
-    domain_df.to_csv(f'{domain_folder}\Cleaned_{domain_file_basename}',
-                     index=False)
-
-    return domain_df
-
-
-def get_non_matches(domain_df, pattern_file):
-    '''This Function returns a Dataframes with all unmatched elements in the Pattern file'''
-
-    pattern_df = pd.DataFrame()
-
-    # Reads the Pattern file in chunks, and them get non matches against the domain file
-    for chunk in pd.read_csv(pattern_file, header=None, usecols=[0], dtype='category', chunksize=200000):
-
-        chunk.iloc[:, 0] = chunk.iloc[:, 0].str.lower()
-
-        chunk = chunk.merge(domain_df, how='left', indicator=True)
-
-        chunk = chunk[chunk['_merge'] == 'left_only']
-
-        chunk.drop('_merge', axis=1, inplace=True)
-
-        pattern_df = pd.concat([pattern_df, chunk], ignore_index=True, axis=0)
-
-    return pattern_df
-
-
-def merge_json(domain_file, domain_folder, domain_file_basename):
-    '''Merges all json files on the Domain Folder'''
-
-    final_json = os.path.join(
-        domain_folder, domain_file_basename[:-4] + '.json')
-
-    if os.path.exists(final_json):
-
-        os.remove(final_json)
-
-    for pattern_json in os.listdir(domain_folder):
-
-        if pattern_json.endswith('.json') and pattern_json != os.path.basename(final_json):
-
-            with open(os.path.join(domain_folder, pattern_json), 'r') as infile,\
-                    open(final_json, 'a') as outfile:
-
-                logging.debug(f'Writing {pattern_json} into merged json')
-
-                for line in infile:
-
-                    try:
-
-                        result = json.loads(line)
-                        json.dump(result, outfile)
-
-                    except:
-
-                        pass
-
-                infile.close()
-
-                os.remove(os.path.join(domain_folder, pattern_json))
-
-
-def break_domain_path(domain_file):
-    return os.path.dirname(domain_file), os.path.basename(domain_file)
-
-
-def valid_pattern_file(pattern_file):
-    '''Checks is file is a CSV'''
-    return pattern_file.endswith('.csv')
-
 
 if __name__ == "__main__":
 
-    # Creates log file and shows logs on console as well
-    logging.basicConfig(format='%(asctime)s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M',
-                        level=logging.DEBUG,
-                        handlers=[logging.FileHandler('BigData.log'),
-                                  logging.StreamHandler()])
+    for domain_file_path in domain_list:
 
-    # Iterates through all files on the Domain File list
-    for domain_file in domain_list:
-
-        domain_folder, domain_file_basename = break_domain_path(domain_file)
-
-        domain_file_basename = os.path.basename(domain_file)
-        domain_folder = os.path.dirname(domain_file)
-
-        logging.debug(f'Cleaning {domain_file_basename} ...')
+        domain_folder, domain_file_basename = break_domain_path(
+            domain_file_path)
 
         domain_df = clean_domain_df(
-            domain_file, domain_file_basename, domain_folder)
+            domain_file_path, domain_file_basename, domain_folder)
 
-        logging.debug(f'{domain_file_basename} Cleaned!!!')
-
-        # Lists all files on the Pattern Folder and iterates through them
         for pattern_file in os.listdir(pattern_folder):
 
-            if valid_pattern_file(pattern_file):
-
-                pattern = os.path.basename(pattern_file)[:-4]
-
-                logging.debug(f'Looking for non matches in {pattern} file...')
+            if pattern_file_isvalid(pattern_file):
 
                 pattern_df = get_non_matches(
                     domain_df, os.path.join(pattern_folder, pattern_file))
 
-                pattern_df.columns = [f'{pattern}']
+                create_json(domain_folder, pattern_file, pattern_df)
 
-                logging.debug(f'Matching for {pattern} file finished')
-
-                with open(f'{domain_folder}\{pattern_file[:-4]}.json', 'w') as f:
-                    json.dump(pattern_df.to_dict(orient='list'), f)
-
-            else:
-
-                logging.debug(f'File {pattern_file} is not a CSV!!')
-
-        merge_json(domain_file)
+        merge_json(domain_folder, domain_file_basename)
---
+++ functions.py
@@ -0,0 +1,122 @@
+
+import os
+import logging
+import json
+import pandas as pd
+
+
+def chunk_processing(chunk):
+    chunk.drop_duplicates(inplace=True)
+    chunk = chunk[chunk.str.len() <= length]
+    chunk = chunk.str[:-5]
+    return chunk
+
+
+def chunk_match(chunk):
+
+    chunk.iloc[:, 0] = chunk.iloc[:, 0].str.lower()
+
+    chunk = chunk.merge(domain_df, how='left', indicator=True)
+
+    chunk = chunk[chunk['_merge'] == 'left_only']
+
+    chunk.drop('_merge', axis=1, inplace=True)
+
+    return chunk
+
+
+def clean_domain_df(domain_file_path, domain_file_basename, domain_folder):
+    '''This function cleans the domain file, saving a CSV and returning a dataframe.'''
+
+    logging.debug(f'Cleaning {domain_file_basename} ...')
+
+    domain_df = pd.DataFrame()
+
+    for chunk in pd.read_csv(domain_file_path, sep='\t', usecols=[0], dtype='category', chunksize=200000, squeeze=True):
+
+        chunk = chunk_processing(chunk)
+
+        domain_df = pd.concat([domain_df, chunk], ignore_index=True, axis=0)
+
+    domain_df.drop_duplicates(inplace=True)
+
+    domain_df.to_csv(f'{domain_folder}\Cleaned_{domain_file_basename}',
+                     index=False)
+
+    logging.debug(f'{domain_file_basename} Cleaned!!!')
+
+    return domain_df
+
+
+def get_non_matches(domain_df, pattern_file):
+    '''This Function returns a Dataframes with all unmatched elements in the Pattern file'''
+
+    pattern = os.path.basename(pattern_file)[:-4]
+
+    logging.debug(f'Looking for non matches in {pattern} file...')
+
+    pattern_df = pd.DataFrame()
+
+    for chunk in pd.read_csv(pattern_file, header=None, usecols=[0], dtype='category', chunksize=200000):
+
+        chunk = chunk_match(chunk)
+
+        pattern_df = pd.concat([pattern_df, chunk], ignore_index=True, axis=0)
+
+    pattern_df.columns = [f'{pattern}']
+
+    logging.debug(f'Matching for {pattern} file finished')
+
+    return pattern_df
+
+
+def create_json(domain_folder, pattern_file, pattern_df):
+    with open(f'{domain_folder}\{pattern_file[:-4]}.json', 'w') as f:
+        json.dump(pattern_df.to_dict(orient='list'), f)
+
+
+def merge_json(domain_folder, domain_file_basename):
+    '''Merges all json files on the Domain Folder'''
+
+    final_json = os.path.join(
+        domain_folder, domain_file_basename[:-4] + '.json')
+
+    if os.path.exists(final_json):
+
+        os.remove(final_json)
+
+    for pattern_json in os.listdir(domain_folder):
+
+        if pattern_json.endswith('.json') and pattern_json != os.path.basename(final_json):
+
+            with open(os.path.join(domain_folder, pattern_json), 'r') as infile,\
+                    open(final_json, 'a') as outfile:
+
+                logging.debug(f'Writing {pattern_json} into merged json')
+
+                for line in infile:
+
+                    try:
+
+                        result = json.loads(line)
+                        json.dump(result, outfile)
+
+                    except:
+
+                        pass
+
+                infile.close()
+
+                os.remove(os.path.join(domain_folder, pattern_json))
+
+
+def break_domain_path(domain_file_path):
+    return os.path.dirname(domain_file_path), os.path.basename(domain_file_path)
+
+
+def pattern_file_isvalid(pattern_file):
+    '''Checks is file is a CSV'''
+    valid = pattern_file.endswith('.csv')
+    if not valid:
+        logging.debug(f'File {pattern_file} is not a CSV!!')
+    return valid
